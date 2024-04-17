b94b3fb2896d49e4a273aa430b5a5269c7341d64
Julian Camacho <camachojulian2011@gmail.com>
2024-04-17 01:43:01 -0400

    Direccion de motores lista, falta decorar

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,75 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PIN_A_MOTOR1 5
+#define PIN_B_MOTOR1 6
+#define PIN_A_MOTOR2 9
+#define PIN_B_MOTOR2 10
+
+#define PIN_ADELANTE_MOTOR1 3
+#define PIN_ATRAS_MOTOR1 4
+#define PIN_ADELANTE_MOTOR2 7
+#define PIN_ATRAS_MOTOR2 8
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+
+  pinMode(PIN_A_MOTOR1, OUTPUT);
+  pinMode(PIN_B_MOTOR1, OUTPUT);
+  pinMode(PIN_A_MOTOR2, OUTPUT);
+  pinMode(PIN_B_MOTOR2, OUTPUT);
+
+  pinMode(PIN_ADELANTE_MOTOR1, INPUT_PULLUP);
+  pinMode(PIN_ATRAS_MOTOR1, INPUT_PULLUP);
+  pinMode(PIN_ADELANTE_MOTOR2, INPUT_PULLUP);
+  pinMode(PIN_ATRAS_MOTOR2, INPUT_PULLUP);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  
+  if (digitalRead(PIN_ADELANTE_MOTOR1) == LOW && digitalRead(PIN_ATRAS_MOTOR1) == HIGH)
+  {
+    Serial.println("Motor 1 adelante");
+    digitalWrite(PIN_A_MOTOR1, HIGH);
+    digitalWrite(PIN_B_MOTOR1, LOW);
+  }
+  else if (digitalRead(PIN_ADELANTE_MOTOR1) == HIGH && digitalRead(PIN_ATRAS_MOTOR1) == LOW)
+  {
+    Serial.println("Motor 1 atras");
+    digitalWrite(PIN_A_MOTOR1, LOW);
+    digitalWrite(PIN_B_MOTOR1, HIGH);
+  }
+  else
+  {
+    //Serial.println("Motor 1 detenido");
+    digitalWrite(PIN_A_MOTOR1, LOW);
+    digitalWrite(PIN_B_MOTOR1, LOW);
+  }
+
+  if (digitalRead(PIN_ADELANTE_MOTOR2) == LOW && digitalRead(PIN_ATRAS_MOTOR2) == HIGH)
+  {
+    Serial.println("Motor 2 adelante");
+    digitalWrite(PIN_A_MOTOR2, HIGH);
+    digitalWrite(PIN_B_MOTOR2, LOW);
+  }
+  else if (digitalRead(PIN_ADELANTE_MOTOR2) == HIGH && digitalRead(PIN_ATRAS_MOTOR2) == LOW)
+  {
+    Serial.println("Motor 2 atras");  
+    digitalWrite(PIN_A_MOTOR2, LOW);
+    digitalWrite(PIN_B_MOTOR2, HIGH);
+  }
+  else
+  {
+    //Serial.println("Motor 2 detenido");
+    digitalWrite(PIN_A_MOTOR2, LOW);
+    digitalWrite(PIN_B_MOTOR2, LOW);
+  }
+   
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
